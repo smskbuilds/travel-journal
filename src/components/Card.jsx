b0dfd9abb46941892f4242fa56d9863f41feac84
smskbuilds <smskbuilds@gmail.com>
2024-07-12 11:15:51 -0700

    Added Card Component, Styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,19 @@
+import pin from '../assets/pin.png'
+
 export default function Card() {
     return (
-        <h1>Card!</h1>
+        <div className="card--container">
+            <img src="https://i.natgeofe.com/n/1488e878-fe1d-4ce7-b6f5-16ce19435851/cherry-blossoms-mt-fuji-japan_3x4.jpg" className="card--main--picture" />
+            <div className="card--details">
+                <div className="card--location">
+                    <img src={pin} alt="" className="card--pin" />
+                    <h5 className="card--country">JAPAN</h5>
+                    <a href="" className="card--map--link">View on Google Maps</a>
+                </div>
+                <h2>Mount Fuji</h2>
+                <h4>12 Jan, 2021 - 24 Jan, 2021</h4>
+                <p>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+            </div>
+        </div>
     )
 }
