594372510105d0981e870865b4f485a05fc5ef9c
smskbuilds <smskbuilds@gmail.com>
2024-07-12 13:54:02 -0700

    Card Component now Reads from Props, Builds from Array of Objects

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,26 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from './components/Header'
 import Card from './components/Card'
+import data from './data.jsx'
 
 
 function App() {
+  const cards = data.map((card) => 
+  <Card 
+    title = {card.title}
+    location = {card.location}
+    googleMapsUrl = {card.googleMapsUrl}
+    startDate = {card.startDate}
+    endDate = {card.endDate}
+    description = {card.description}
+    imageUrl = {card.imageUrl}
+  />
+  )
 
   return (
       <div className="app--container">
         <Header />
-        <Card />
+        {cards}
       </div>
   )
 }
--- src/data.jsx
+++ src/data.jsx
@@ -1,11 +1,29 @@
 export default [
     {
-        title: "Mount Fuji",
-        location: "Japan",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
+        title: "Zion National Park",
+        location: "U.S.A.",
+        googleMapsUrl: "https://maps.app.goo.gl/MkiZsmVtEGfcTUAr8",
+        startDate: "20 Apr, 2018",
+        endDate: "30 Apr, 2018",
+        description: "Zion National Park, located in southwestern Utah, is renowned for its stunning sandstone cliffs, diverse ecosystems, and deep, narrow canyons carved by the Virgin River. The park offers a variety of outdoor activities, including hiking, rock climbing, and wildlife viewing, attracting millions of visitors each year.",
+        imageUrl: "https://scontent-lax3-2.xx.fbcdn.net/v/t31.18172-8/30856687_10160427179355217_1055756450651193477_o.jpg?_nc_cat=107&ccb=1-7&_nc_sid=13d280&_nc_ohc=FSBubEnxIzEQ7kNvgE__47r&_nc_ht=scontent-lax3-2.xx&oh=00_AYBWPdrTts4aUaOZfJdn0IfOYVgBFGFqEtel20t8d41Wtw&oe=66B8FAEB",
+    },
+    {
+        title: "Big Sur",
+        location: "U.S.A.",
+        googleMapsUrl: "https://maps.app.goo.gl/AyCR692sfZ69g8N57",
+        startDate: "1 Jan, 2023",
+        endDate: "10 Jan, 2023",
+        description: "Big Sur, located along California's central coast, is famed for its dramatic cliffs, rugged coastline, and breathtaking ocean views along the scenic Pacific Coast Highway. This picturesque region offers a blend of natural beauty and outdoor recreation, including hiking trails through redwood forests and picturesque beaches.",
+        imageUrl: "https://scontent-lax3-1.xx.fbcdn.net/v/t39.30808-6/324029656_463719359308907_3233639053632883325_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=fZ5sApTAUDMQ7kNvgHHcIYi&_nc_ht=scontent-lax3-1.xx&oh=00_AYD9-I-11vJqcwlVemYjxis2_vjgzmwHc9zRdAaXJyRx0A&oe=66976E46",
+    },
+    {
+        title: "Mt. Cook National Park",
+        location: "New Zealand",
+        googleMapsUrl: "https://maps.app.goo.gl/6jqnAjRAk7oX2ZvZ6",
+        startDate: "1 Jan, 2020",
+        endDate: "10 Jan, 2020",
+        description: "Mount Cook National Park, located in New Zealand's South Island, encompasses the country's highest peaks, including Mount Cook (Aoraki), and is renowned for its stunning alpine scenery and glaciers. The park offers a range of outdoor activities, such as hiking, mountaineering, and stargazing, attracting nature enthusiasts and adventure seekers from around the world.",
+        imageUrl: "https://i.imgur.com/R2n4Hny.jpeg",
     }
 ]
